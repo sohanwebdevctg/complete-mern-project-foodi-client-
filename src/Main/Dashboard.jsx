08b023a75146427609ec95c3522f4da1cf91b0d7
sohanwebdevctg <amirhossainsohan0@gmail.com>
2025-02-12 16:03:20 +0600

    adding drawer and side bar

--- src/Main/Dashboard.jsx
+++ src/Main/Dashboard.jsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
 
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>hello</h1>
-      <Outlet></Outlet>
-    </div>
+    <div className="drawer drawer-open">
+  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content w-full">
+    {/* Page content here */}
+    <Outlet></Outlet>
+    {/* Page content here */}
+  </div>
+  <div className="drawer-side">
+    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu bg-slate-200 text-base-content min-h-full w-56 p-4">
+      {/* Sidebar content here */}
+      <li><Link to="/dashboard/updateProfile">UpdateProfile</Link></li>
+      <li><Link to="/dashboard/allUser">AllUser</Link></li>
+    </ul>
+  </div>
+</div>
   );
 };
 
